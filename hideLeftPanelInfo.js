1aecd5d6e13fe658a47aa065aa5df1fe0d439827
Dylan <dylan.shaw@tealium.com>
2018-08-27 15:40:17 -0700

    built out hideLeftPanel file

---
+++ hideLeftPanelInfo.js
@@ -0,0 +1,16 @@
+try {
+    function hideLeftPanelInfo(container) {
+        container.find('.contextBox').find('.contextActionButtons').nextAll().remove();
+        container.find('.rating-container').remove();
+    }
+    utui.util.pubsub.subscribe(utui.constants.profile.LOADED, function() {
+        utui.util.pubsub.subscribe('accordion_expanded_tag', function(e) {
+            var $container = $('#' + e.container);
+            if(features.hideLeftPanelInfo.enabled){
+                hideLeftPanelInfo($container)
+            }
+        });
+    });
+} catch(e){
+    console.log('Hide Left Panel Info failed: ' + e);
+}
