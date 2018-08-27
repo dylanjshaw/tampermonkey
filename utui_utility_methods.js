29a8063847e1ade46c2555be9282aa598a775875
Dylan <dylan.shaw@tealium.com>
2018-08-27 16:35:05 -0700

    built out utui utility methods file

---
+++ utui_utility_methods.js
@@ -0,0 +1,24 @@
+utui.util.retrieveTemplateCode = (uid, el, template_ref) => {
+    utui.service.get(utui.service.restapis.GET_TEMPLATE,
+        {
+            account: utui.data.settings.account,
+            profile: utui.data.settings.profileid,
+            revision: utui.data.settings.revision,
+            template: template_ref,
+            cb: Math.random()
+        },
+        null,
+        (data) => {
+            el.innerHTML = data.content;
+        }
+    )
+}
+
+utui.util.copyToClipboard = (element) => {
+    var $temp = $("<textarea>");
+    var brRegex = /<br\s*[\/]?>/gi;
+    $("body").append($temp);
+    $temp.val($(element).text().replace(brRegex, "\r\n")).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
