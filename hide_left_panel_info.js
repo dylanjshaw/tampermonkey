6f9526d4563720f5577b98e831e1cbe7e93e841c
Dylan <dylan.shaw@tealium.com>
2018-08-27 15:50:40 -0700

    deleted local copy of file and removed features check

--- hide_left_panel_info.js
+++ hide_left_panel_info.js
@@ -5,8 +5,6 @@ function hideLeftPanelInfo(container) {
 utui.util.pubsub.subscribe(utui.constants.profile.LOADED, function() {
     utui.util.pubsub.subscribe('accordion_expanded_tag', function(e) {
         var $container = $('#' + e.container);
-        if(features.hideLeftPanelInfo.enabled){
             hideLeftPanelInfo($container)
-        }
     });
 });
