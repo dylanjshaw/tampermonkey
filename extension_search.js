63408b964d2de3d6248325b9b197628e3fece8dd
Dylan <dylan.shaw@tealium.com>
2018-09-02 16:30:52 -0700

    built out extension search file

---
+++ extension_search.js
@@ -0,0 +1,57 @@
+utui.util.pubsub.subscribe(utui.constants.profile.LOADED, function() {
+
+    localStorage.setItem("extensionSearchQuery", "");
+
+    function searchExtensions(string) {
+        // console.log('Running a search for "'+string+'"');
+        localStorage.setItem("extensionSearchQuery", string);
+        var re = new RegExp(string, 'i');
+        var data = utui.data.customizations;
+        var extensions = {};
+        if (string !== '') {
+            Object.keys(data).forEach(function(id) {
+                // console.log(data[id]);
+                var extension = data[id];
+                Object.keys(extension).forEach(function(key) {
+                    // console.log(key,extension[key]);
+                    var extData = extension[key];
+                    if (key != '_id' && key != 'id' && key != 'labels' && key != 'scope' && key != 'scopevars' && key != 'sort' && key != 'status' && key != 'type' && key != '_ignoreError' && !key.match(/_setoption/) && key != 'settings') {
+                        if (typeof extData === 'string' && extData.match(re)) {
+                            extensions[extension.sort] = 1;
+                        }
+                    }
+                });
+            });
+        }
+        $('#customize_content .customize_container').each(function(i) {
+            if (extensions[i] == 1) {
+                $(this).find('h3').css('background-color', '#fad52d');
+            } else {
+                $(this).find('h3').css('background-color', '');
+            }
+        });
+    }
+
+    function setupExtensionSearch() {
+        if (!$('input#extension-search').length) {
+            var searchTerm = localStorage.getItem("extensionSearchQuery") || '';
+            $('<div class="inputSearchContainer tmui"><input class="search" id="extension-search" value="' + searchTerm + '" type="text"></div>')
+                .css('float', 'right')
+                .css('z-index', '1')
+                .appendTo('#tabs-customizations .config_button_nofloat');
+            var keysPressed = 0;
+            $('input#extension-search').bind('keydown', function() {
+                // console.log('keydown');
+                var grabKeyCount = ++keysPressed;
+                setTimeout(function() {
+                    if (keysPressed == grabKeyCount) {
+                        searchExtensions($('input#extension-search').val());
+                    }
+                }, 250);
+            });
+            searchExtensions($('input#extension-search').val());
+        }
+    }
+
+    setupExtensionSearch()
+})
