ce45705bfb65f9a74479fdd1e77143fc0d45d556
Dylan <dylan.shaw@tealium.com>
2018-09-12 12:40:14 -0700

    revised bulk lr import file

--- bulk_load_rules.js
+++ bulk_load_rules.js
@@ -1,10 +1,9 @@
 utui.util.pubsub.subscribe(utui.constants.profile.LOADED, function() {
         var bulk_load_rules_import = function() {
-            console.log('Bulk Load Rules Loading');
             var bulk_add_link = $('<div class="tab-menu-item tmui" style="float:right;"><span id="loadrules_button_import" class="btn btn-success" style="margin-top:0;"><i class="icon-plus"></i><span> Bulk Import</span></span></div>');
             var import_container = $('<div id="load_rules_container" class="ui-dialog ui-widget ui-widget-content ui-corner-all" tabindex="-1" role="dialog" aria-labelledby="ui-dialog-title-admin_dialog"><div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix"><span class="ui-dialog-title">Bulk Add Load Rules</span><a href="#" class="ui-dialog-titlebar-close ui-corner-all" role="button"><span class="ui-icon ui-icon-closethick load_rules_btn"></span></a></div><div style="width: auto; height: auto;" class="ui-dialog-content ui-widget-content" scrolltop="0" scrollleft="0"><textarea id="csv_load_rules" cols="11" rows="25" style="margin-left:auto;display: block;margin-right: auto;margin-top: auto;margin-bottom: auto;"></textarea></div><div class="ui-dialog-buttonpane ui-widget-content ui-helper-clearfix"><div class="ui-dialog-buttonset"><button type="button" id="add_load_rules" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button" aria-disabled="false" original-title="Click to import the load rules." title="Click to import the load rules."><span class="ui-button-text">Import</span></button><button type="button" style="float:left; margin-left: 12px" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button" aria-disabled="false"><span class="ui-button-text load_rules_btn">Close</span></button></div></div></div>');
             $(import_container).css({'z-index': '1002', 'outline': '0px','height': 'auto','top': '200px','margin': 'auto auto auto 20%','width': '50%','min-width': '400px', 'display':'none'});
-            $("#loadrulesContainer_headerControls").append(bulk_add_link);
+            if($("#loadrulesContainer_headerControls").find('#loadrules_button_import').length == 0){$("#loadrulesContainer_headerControls").append(bulk_add_link);}
             $("#tabs").append(import_container);
             $(".load_rules_btn").click(function() {
                 $("#csv_load_rules").val("");
@@ -53,7 +52,6 @@ utui.util.pubsub.subscribe(utui.constants.profile.LOADED, function() {
                     alert("Field cannot be blank!");
                 }
             });
-            console.log('Bulk Load Rules Loaded');
         }
         bulk_load_rules_import();
 })
